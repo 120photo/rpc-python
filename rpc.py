1c12d518799fd448914ae5071fa2da1b83db5cf0
Jose Juarez <jose@juarez.me>
2016-11-29 17:25:33 -0800

    initial commit, setup base program

---
+++ rpc.py
@@ -0,0 +1,12 @@
+from os import system
+from random import randint
+
+system('clear')
+
+# Variable Declarations
+weapons = {1:"rock", 2:"paper", 3:"scissors"}
+greeting = "Welcome to the Rock Paper Scissors Challenge"
+
+print("+" * (len(greeting) + 2))
+print(greeting)
+print("+" * (len(greeting) + 2))
