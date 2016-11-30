f55ef853dafc0292a6c6d668d5462326774ac62e
Jose Juarez <jose@juarez.me>
2016-11-29 17:43:58 -0800

    organizing code into a function, getting user input and making sure it a valid option

--- rpc.py
+++ rpc.py
@@ -5,8 +5,30 @@ system('clear')
 
 # Variable Declarations
 weapons = {1:"rock", 2:"paper", 3:"scissors"}
-greeting = "Welcome to the Rock Paper Scissors Challenge"
+greeting = "Welcome to the Rock Paper Scissors Challenge."
+computer_guess = randint(1,3)
 
-print("+" * (len(greeting) + 2))
-print(greeting)
-print("+" * (len(greeting) + 2))
+def the_game():
+    '''this is where we play'''
+    print("+" * (len(greeting) + 2))
+    print(greeting)
+    print("+" * (len(greeting) + 2))
+    # Menue of options availabe
+    for key,value in weapons.items():
+        print("{} to play {}.".format(key,value.capitalize()))
+    # User makes their choice
+    user_play = int(input("Choose Your Weapon: "))
+    if user_play not in [1,2,3]:
+        print("Not a valid option, chose again")
+        the_game()
+
+the_game()
+
+if computer_guess == 1:
+    print("Computer Played {}".format(weapons[computer_guess]))
+
+if computer_guess == 2:
+    print("Computer Played {}".format(weapons[computer_guess]))
+
+if computer_guess == 3:
+    print("Computer Played {}".format(weapons[computer_guess]))
